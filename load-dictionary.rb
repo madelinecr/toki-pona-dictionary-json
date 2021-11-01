524c323092d45710df64cf606ba42b3f76c78677
Avery Pace <blainepace@gmail.com>
2021-11-01 17:05:38 -0400

    Finish converting to JSON, include JSON output in repository

--- load-dictionary.rb
+++ load-dictionary.rb
@@ -1,28 +1,9 @@
 #!/usr/bin/env ruby
 
-dict_file = File.open("./tokipona-formatted.txt")
-dict_file_contents = dict_file.read.split("\n")
+require 'json'
+require 'pp'
 
-dictionary = {}
-
-word = nil
-
-dict_file_contents.each do |line|
-    if line =~ /^\w+/
-        word = line
-    elsif line =~ /^\s+/
-        definition = line.strip
-        if dictionary[word] == nil
-            dictionary[word] = []
-        end
-
-        dictionary[word].append(definition)
-    else
-        puts "ERROR READING DICTIONARY FILE"
-        exit
-    end
-end
-
+# PARTS OF SPEECH
 parts_of_speech_file = File.open("./tokipona-partsofspeech.txt")
 parts_of_speech_contents = parts_of_speech_file.read.split("\n")
 
@@ -42,14 +23,57 @@ parts_of_speech_contents.each do |line|
     end
 end
 
-puts "DICTIONARY:"
 
-dictionary.each do |k,v|
-    puts "#{k}: #{v}"
+# DEFINITIONS
+dict_file = File.open("./tokipona-formatted.txt")
+dict_file_contents = dict_file.read.split("\n")
+
+definitions_dict = {}
+
+word = nil
+
+dict_file_contents.each do |line|
+    if line =~ /^\w+/
+        word = line
+    elsif line =~ /^\s+/
+        definition = line.strip
+        if definitions_dict[word] == nil
+            definitions_dict[word] = []
+        end
+
+        definitions_dict[word].append(definition)
+    else
+        puts "ERROR READING DICTIONARY FILE"
+        exit
+    end
 end
 
-puts "PARTS OF SPEECH:"
 
-pos_dictionary.each do |k,v|
-    puts "#{k}: #{v}"
+# BUILDING ENTIRE DICTIONARY INTO JSON-ABLE STRUCTURE
+dictionary = {}
+
+definitions_dict.each do |word,definitions|
+    definitions_array = []
+    definitions.each do |definition|
+        value_matched = false
+        pos_dictionary_checked = false
+        pos_dictionary.each do |pos,pos_definition|
+            if definition.start_with?(pos) and value_matched == false
+                trimmed_definition = definition.sub(/^#{pos}\s/, '')
+                definitions_array.append({ pos.to_sym => trimmed_definition })
+                value_matched = true
+            end
+        end
+        pos_dictionary_checked = true
+        if value_matched == false and pos_dictionary_checked == true
+            definitions_array.append({ extra: definition })
+        end
+    end
+    dictionary[word.to_sym] = definitions_array
 end
+
+dict_output = File.open("./toki-dictionary.json", "wb")
+dict_output.write(JSON.pretty_generate(dictionary))
+
+pos_output = File.open("./toki-partsofspeech.json", "wb")
+pos_output.write(JSON.pretty_generate(pos_dictionary))
